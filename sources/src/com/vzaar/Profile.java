d2bbb9c0ef86f9b9dac5cfc132261ecf582f493c
skitsanos <info@skitsanos.com>
2014-02-26 11:58:19 +0200

    removed source  files

--- sources/src/com/vzaar/Profile.java
+++
@@ -1,9 +0,0 @@
-package com.vzaar;
-
-public class Profile {
-    public static final int Small = 1;
-    public static final int Medium = 2;
-    public static final int Large = 3;
-    public static final int HighDefinition = 4;
-    public static final int Original = 5;
-}
