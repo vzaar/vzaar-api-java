a1dfc76c237b18663547c6b3189f78ab708125c6
Marc <marc.g.smith@gmail.com>
2017-01-15 23:58:22 +0000

    Added secure token

--- src/integration-test/groovy/com/vzaar/BaseIntegrationSpec.groovy
+++ src/integration-test/groovy/com/vzaar/BaseIntegrationSpec.groovy
@@ -7,7 +7,6 @@ public class BaseIntegrationSpec extends Specification {
     protected Vzaar vzaar
     def setup() {
         ObjectMapperFactory.setFailOnUnknownProperties(true)
-        println "VzaarClientId=${System.getenv("vzaarClientId")}"
         vzaar = Vzaar.make(
                 System.getenv("vzaarClientId"),
                 System.getenv("vzaarAuthToken"))
