77c86fbe0cdbd1c4d7ae897d3ba100219416aef1
Marc <marc.g.smith@gmail.com>
2017-01-15 23:52:50 +0000

    Force jdk8 on travis

--- src/integration-test/groovy/com/vzaar/BaseIntegrationSpec.groovy
+++ src/integration-test/groovy/com/vzaar/BaseIntegrationSpec.groovy
@@ -7,6 +7,7 @@ public class BaseIntegrationSpec extends Specification {
     protected Vzaar vzaar
     def setup() {
         ObjectMapperFactory.setFailOnUnknownProperties(true)
+        println "VzaarClientId=${System.getenv("vzaarClientId")}"
         vzaar = Vzaar.make(
                 System.getenv("vzaarClientId"),
                 System.getenv("vzaarAuthToken"))
